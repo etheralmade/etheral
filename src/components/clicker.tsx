622aade209c21912ac4c97673bdfea4c527c29dc
Louis Andrew <louisandrew3@gmail.com>
2020-08-22 20:03:21 +0200

    got an issue here lol https://github.com/gatsbyjs/gatsby/issues/1818

--- src/components/clicker.tsx
+++ src/components/clicker.tsx
@@ -14,53 +14,55 @@ const Clicker: React.FC<{ products: any }> = ({ products }) => {
     const dispatch = useDispatch();
 
     // Function to fetch data from the firestore AND transforming the refernce into an actual imguRl
-    const fetchDatas = async () => {
-        await dispatch(fetchProducts());
+    // const fetchDatas = async () => {
+    //     await dispatch(fetchProducts());
 
-        try {
-            const req = await db()
-                .collection('fl_content')
-                .where('_fl_meta_.schema', '==', 'product')
-                .get();
+    //     try {
+    //         const req = await db()
+    //             .collection('fl_content')
+    //             .where('_fl_meta_.schema', '==', 'product')
+    //             .get();
 
-            const rsp = await Promise.all(
-                req.docs.map(async doc => {
-                    const data = doc.data();
-                    if (await data.image) {
-                        const reqImg = await Promise.all(
-                            data.image.map((ref: any) =>
-                                ref.get().then((imgDoc: any) => imgDoc.data())
-                            )
-                        );
+    //         const rsp = await Promise.all(
+    //             req.docs.map(async doc => {
+    //                 const data = doc.data();
+    //                 if (await data.image) {
+    //                     const reqImg = await Promise.all(
+    //                         data.image.map((ref: any) =>
+    //                             ref.get().then((imgDoc: any) => imgDoc.data())
+    //                         )
+    //                     );
 
-                        const imgDownloadUrls: any = await Promise.all(
-                            reqImg.map(async (ref: any) => {
-                                try {
-                                    const downloadUrlReq = await storage()
-                                        .ref(`flamelink/media/${ref.file}`)
-                                        .getDownloadURL();
+    //                     const imgDownloadUrls: any = await Promise.all(
+    //                         reqImg.map(async (ref: any) => {
+    //                             try {
+    //                                 const downloadUrlReq = await storage()
+    //                                     .ref(`flamelink/media/${ref.file}`)
+    //                                     .getDownloadURL();
 
-                                    return await downloadUrlReq;
-                                } catch (err) {
-                                    console.log(err);
-                                    return '';
-                                }
-                            })
-                        );
+    //                                 return await downloadUrlReq;
+    //                             } catch (err) {
+    //                                 console.log(err);
+    //                                 return '';
+    //                             }
+    //                         })
+    //                     );
 
-                        await set(data, 'productImage', imgDownloadUrls);
-                    }
+    //                     await set(data, 'productImage', imgDownloadUrls);
+    //                 }
 
-                    return data;
-                })
-            );
+    //                 return data;
+    //             })
+    //         );
 
-            await dispatch(storeProducts(rsp));
-        } catch (err) {
-            console.log('something went wrong');
-            console.log(err);
-        }
-    };
+    //         await dispatch(storeProducts(rsp));
+    //     } catch (err) {
+    //         console.log('something went wrong');
+    //         console.log(err);
+    //     }
+    // };
+
+    const fetchDatas = () => {};
 
     const transformImges = async () => {
         console.log(products);
