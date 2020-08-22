8eb69c980121a7e55dbc337ff184bf88bd673743
Louis Andrew <louisandrew3@gmail.com>
2020-08-22 20:13:29 +0200

    Trying to fix async/await bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,13 @@
 exports.modifyBabelrc = ({ babelrc }) => ({
     ...babelrc,
-    ...(process.env.NODE_ENV !== 'development' && {
-        plugins: babelrc.plugins.concat([
-            'transform-regenerator',
-            'transform-runtime',
-        ]),
-    }),
+    // ...(process.env.NODE_ENV !== 'development' && {
+    //     plugins: babelrc.plugins.concat([
+    //         'transform-regenerator',
+    //         'transform-runtime',
+    //     ]),
+    // }),
+    plugins: babelrc.plugins.concat([
+        'transform-regenerator',
+        'transform-runtime',
+    ]),
 });
--- src/components/clicker.tsx
+++ src/components/clicker.tsx
@@ -4,6 +4,7 @@ import { useDispatch, connect } from 'react-redux';
 import { set } from 'lodash';
 import firebase from 'gatsby-plugin-firebase';
 
+import 'regenerator-runtime/runtime';
 import { fetchProducts, storeProducts } from 'state/actions/products';
 
 // Mock component to simulate how the data is being fetched from firebase (data is provided by flamelink and needed special
@@ -14,55 +15,53 @@ const Clicker: React.FC<{ products: any }> = ({ products }) => {
     const dispatch = useDispatch();
 
     // Function to fetch data from the firestore AND transforming the refernce into an actual imguRl
-    // const fetchDatas = async () => {
-    //     await dispatch(fetchProducts());
+    const fetchDatas = async () => {
+        await dispatch(fetchProducts());
 
-    //     try {
-    //         const req = await db()
-    //             .collection('fl_content')
-    //             .where('_fl_meta_.schema', '==', 'product')
-    //             .get();
+        try {
+            const req = await db()
+                .collection('fl_content')
+                .where('_fl_meta_.schema', '==', 'product')
+                .get();
 
-    //         const rsp = await Promise.all(
-    //             req.docs.map(async doc => {
-    //                 const data = doc.data();
-    //                 if (await data.image) {
-    //                     const reqImg = await Promise.all(
-    //                         data.image.map((ref: any) =>
-    //                             ref.get().then((imgDoc: any) => imgDoc.data())
-    //                         )
-    //                     );
+            const rsp = await Promise.all(
+                req.docs.map(async doc => {
+                    const data = doc.data();
+                    if (await data.image) {
+                        const reqImg = await Promise.all(
+                            data.image.map((ref: any) =>
+                                ref.get().then((imgDoc: any) => imgDoc.data())
+                            )
+                        );
 
-    //                     const imgDownloadUrls: any = await Promise.all(
-    //                         reqImg.map(async (ref: any) => {
-    //                             try {
-    //                                 const downloadUrlReq = await storage()
-    //                                     .ref(`flamelink/media/${ref.file}`)
-    //                                     .getDownloadURL();
+                        const imgDownloadUrls: any = await Promise.all(
+                            reqImg.map(async (ref: any) => {
+                                try {
+                                    const downloadUrlReq = await storage()
+                                        .ref(`flamelink/media/${ref.file}`)
+                                        .getDownloadURL();
 
-    //                                 return await downloadUrlReq;
-    //                             } catch (err) {
-    //                                 console.log(err);
-    //                                 return '';
-    //                             }
-    //                         })
-    //                     );
+                                    return await downloadUrlReq;
+                                } catch (err) {
+                                    console.log(err);
+                                    return '';
+                                }
+                            })
+                        );
 
-    //                     await set(data, 'productImage', imgDownloadUrls);
-    //                 }
+                        await set(data, 'productImage', imgDownloadUrls);
+                    }
 
-    //                 return data;
-    //             })
-    //         );
+                    return data;
+                })
+            );
 
-    //         await dispatch(storeProducts(rsp));
-    //     } catch (err) {
-    //         console.log('something went wrong');
-    //         console.log(err);
-    //     }
-    // };
-
-    const fetchDatas = () => {};
+            await dispatch(storeProducts(rsp));
+        } catch (err) {
+            console.log('something went wrong');
+            console.log(err);
+        }
+    };
 
     const transformImges = async () => {
         console.log(products);
