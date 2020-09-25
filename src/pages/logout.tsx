57bd0b72b913a6d17be8f92d5c52a722b02e5c88
Louis Andrew <louisandrew3@gmail.com>
2020-09-25 19:18:19 +0200

    updated failing build

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageProps } from 'gatsby';
 import { Layout } from 'components/layout';
 
 import firebase from 'gatsby-plugin-firebase';
 
 const Logout = (props: PageProps) => {
-    const auth = firebase.auth();
+    const [auth, setAuth] = useState<firebase.auth.Auth | undefined>(undefined);
 
-    return (
+    useEffect(() => {
+        setAuth(firebase.auth());
+    }, []);
+
+    return auth ? (
         <Layout>
             <div className="content">
                 <h1>User: {auth.currentUser?.displayName}</h1>
                 <button onClick={() => auth.signOut()}>Logout</button>
             </div>
         </Layout>
-    );
+    ) : null;
 };
 
 export default Logout;
