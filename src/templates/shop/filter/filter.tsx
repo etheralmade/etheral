5e17408bff37f16ef9ccde33e324ae9850549cef
Louis Andrew <louisandrew3@gmail.com>
2020-09-28 12:30:17 +0200

    Initialized filter comopnent

---
+++ src/templates/shop/filter/filter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import styling libs
+// import local components
+
+type Props = {};
+
+const Filter: React.FC<Props> = () => {
+    return <></>;
+};
+
+export { Filter };
