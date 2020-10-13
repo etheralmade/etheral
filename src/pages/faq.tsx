528a35bd615d855c9320b72fce0aefd216440f09
Louis Andrew <louisandrew3@gmail.com>
2020-10-13 16:19:08 +0200

    Added faq and size guide

---
+++ src/pages/faq.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { PageProps, graphql } from 'gatsby';
+
+import { Box } from 'rebass';
+
+import { Layout } from 'components/layout';
+
+const FAQPage = ({ data }: PageProps) => {
+    if (!data) {
+        return null;
+    }
+
+    const { allPages }: any = data as any;
+
+    const pageContent = allPages.edges[0].node.content;
+
+    return (
+        <Layout>
+            <Box className="content" px={[6, 9, 9, '10vw', '13vw']}>
+                <Box dangerouslySetInnerHTML={{ __html: pageContent }} />
+            </Box>
+        </Layout>
+    );
+};
+
+export default FAQPage;
+
+export const query = graphql`
+    query {
+        allPages(filter: { pageName: { eq: "FAQ" } }) {
+            edges {
+                node {
+                    pageName
+                    content
+                }
+            }
+        }
+    }
+`;
